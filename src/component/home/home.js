9719067222f4c51c712cb668c6b09c432c0d3ba0
aymenjn <aymenjani20@gmail.com>
2023-06-14 18:27:51 +0100

    start

---
+++ src/component/home/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as s from "./home.style"
+const  Home = () => {
+  return (
+   <s.Maincontainer>
+    <s.background  src='https://www.banffcentre.ca/sites/default/files/styles/content_feature/public/Media%20and%20Production/DSCN0326_0.JPG?itok=M1YrZwmO' />
+ <s.body>
+    <s.header>
+    new gate 
+    </s.header>
+    
+ </s.body>
+   </s.Maincontainer>
+  )
+}
+
+export default Home
---
+++ src/component/home/home.style.js
@@ -0,0 +1,36 @@
+import styled from "@emotion/styled"
+
+
+export const Maincontainer  = styled.div`
+height : 100vh ; 
+padding : 0 ; 
+margin : 0 ;
+
+`
+export const background = styled.img`
+width :  100% ; 
+position : absolute ; 
+top : 0 ; 
+left : 0 ; 
+height : 100%; 
+z-index : 1 ; 
+`
+export const body = styled.div `
+color : white ; 
+z-index : 100 ;
+font-size : 3rem ;  
+position : absolute ; 
+width : 100% ; 
+top : 0 ; 
+left : 0 ; 
+`
+export const header = styled.div `
+
+width : 100% ; 
+height : 150px ; 
+text-align : center ; 
+backdrop-filter: blur(10px);
+
+
+
+`
